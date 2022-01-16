c0fe7596a6ecd2b3c7478d64c0d98d70e2253773
krosa <krosa100@gmail.com>
2022-01-15 20:44:42 -0600

    Update about_page.py

--- about_page.py
+++ about_page.py
@@ -5,5 +5,5 @@ image = Image.open('img.png')
 
 def show_about_page():
     st.markdown("# :blue_book: About this Project")
-    st.write('This app was built for some purpose and using such and such resources. This app was built for some purpose and using such and such resources.')
+    st.write('This is a BBB app.')
     st.image(image,use_column_width=True)
